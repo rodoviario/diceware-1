ea2cb895fe5edcde8b0f73eda2aa2ca17d7f8e67
Paul Tolstoi <paul.tolstoi@gmail.com>
2013-05-18 14:36:21 +0200

    initial commit

---
+++ DiceWare/DiceWareExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KeePass.Plugins;
+
+namespace DiceWare
+{
+    public sealed class DiceWareExt : Plugin
+    {
+        private IPluginHost m_host;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            m_host = host;
+
+            return true;
+        }
+
+    }
+}
